8aa738371c8f23d25bda3df35fb87f12bd76d34e
NikolnikovaPolina <101321771+NikolnikovaPolina@users.noreply.github.com>
2023-06-04 19:03:42 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using System.Drawing;
 
 using Tao.OpenGl;
 using Tao.FreeGlut;
-using Tao.Platform.Windows;
 
 namespace _3ebba
 {
@@ -38,7 +37,10 @@ namespace _3ebba
             Gl.glOrtho(-W, W, -H, H, -200, 200);
             Gl.glMatrixMode(Gl.GL_MODELVIEW);
             Gl.glLoadIdentity();
-            
+
+            Gl.glEnable(Gl.GL_BLEND);
+            Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
+
             // настройка параметров OpenGL для визуализации
             Gl.glEnable(Gl.GL_DEPTH_TEST);
             Gl.glEnable(Gl.GL_COLOR_MATERIAL);
@@ -51,13 +53,17 @@ namespace _3ebba
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
 
             Gl.glLoadIdentity();
-            Gl.glColor3f(1, 1, 1); // цвет фигуры - белый
+            Gl.glColor4d(1, 1, 1, Convert.ToDouble(trasparency.Text)); // цвет фигуры - белый
 
             Gl.glPushMatrix();
             Gl.glTranslated(-1.8, 1 - Convert.ToDouble(height.Text)/2, 0);
             Gl.glRotated((x - AnT.Height) * 180 / AnT.Height, 0, 1, 0);
             Gl.glRotated((y - AnT.Width) * 180 / AnT.Width, 1, 0, 0);
+           
+            Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
             Glut.glutSolidCone(Convert.ToDouble(radius.Text), Convert.ToDouble(height.Text), 180, 1);
+
+
             Gl.glPopMatrix();
             Gl.glFlush();
             AnT.Invalidate();
